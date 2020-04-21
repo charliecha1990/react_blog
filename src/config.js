04706f1baf07516b22c623e8f3c6a65c9fd99348
charliecha1990 <charliecha1990@gmail.com>
2020-04-21 19:57:04 -0400

    no comment

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ import Href from '@/components/Href'
 import MyInfo from '@/views/web/about/MyInfo'
 
 // API_BASE_URL
-export const API_BASE_URL = 'http://172.105.98.245:6060'
+export const API_BASE_URL = 'http://127.0.0.1:6060'
 
 // export const API_BASE_URL = 'http://127.00.1:6060'
 
